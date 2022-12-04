7bea0cae58ed4db190a4ec852999d0098d6d3799
mhlavac <info@mhlavac.net>
2022-12-04 09:08:40 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,29 @@
+// Day1.txt is expected as an input file
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val input = readInput("Day1")
+
+    var caloriesPerElf = getCaloriesPerElf(input)
+
+    println("Max Calories: " + caloriesPerElf[0])
+    println("Sum of top 3 Calories: " + caloriesPerElf.take(3).sum())
+}
+
+private fun getCaloriesPerElf(input: List<String>): ArrayList<Int> {
+    var caloriesPerElf = ArrayList<Int>()
+    var currentCalories = 0
+
+    input.forEach {
+        if (it == "") {
+            caloriesPerElf.add(currentCalories)
+            currentCalories = 0
+            return@forEach
+        }
+
+        currentCalories += it.toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    caloriesPerElf.sortDescending()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    return caloriesPerElf
 }
